15d07ede3725c1491f41d2d5ff968675a8579e82
Garcia <hfyang.wang@gmail.com>
2021-08-30 15:43:27 +0800

    update index.js and add service-worker.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 //匯入能讓各瀏覽器表現一致的樣式
 import 'normalize.css';
+import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
@@ -18,3 +19,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//將unregister改成register
+serviceWorker.register();
